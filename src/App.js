ff0036beeb2bc4429c65bd14f94709494bee8124
Sarthakjain211001 <56023046+Sarthakjain211001@users.noreply.github.com>
2021-12-13 22:40:06 +0530

    Added the Product.jsx page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Home from './pages/Home';
+import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <div >
       {/* <Home/> */}
-      <ProductList/>
+      {/* <ProductList/> */}
+      <Product/>
     </div>
   );
 }
